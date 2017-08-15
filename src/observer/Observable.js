e2dee36387ecc1788746850015679452f45af9de
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-15 19:46:02 +0200

    no markAsChanged and markAsRead usage inside Observable

--- src/observer/Observable.js
+++ src/observer/Observable.js
@@ -19,37 +19,30 @@ class Observable {
     }
 
     get observed() {
-        this._markAsRead('observed');
         return (this._observer !== null);
     }
 
     get listeners() {
-        this._markAsRead('listeners');
         return (this.observed) ? _observer.listeners : [];
     }
 
     get changed() {
-        this._markAsRead('changed');
         return (this.observed) ? _observer.changed : false;
     }
 
     addListener(listener) {
         if (!this.observed) {
             this._observer = this._handler.createObserver();
-            this._markAsChanged('observed');
         }
         this._observer.addListener(listener);
-        this._markAsChanged('listeners');
         return this;
     }
 
     removeListener(listener) {
         if (this.observed) {
             this._observer.removeListener(listener);
-            this._markAsChanged('listeners');
             if (this._observer.listeners.length == 0) {
                 this._observer = null;
-                this._markAsChanged('observed');
             }
         }
         return this;
@@ -58,9 +51,7 @@ class Observable {
     clearAllChances() {
         if (this.observed) {
             this._observer.clearAllChances();
-            this._markAsChanged('changed');
         }
         return this;
     }
 }
-
