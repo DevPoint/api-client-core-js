6b8698fc6b488da8a2850e1353f9650265660838
Wilfried Reiter <office@devpoint.at>
2017-08-16 08:58:10 +0200

    added dataId

--- src/transaction/UpdateTransaction.js
+++ src/transaction/UpdateTransaction.js
@@ -3,9 +3,15 @@ import Transaction from './Transaction';
 
 class UpdateTransaction extends Transaction {
 
-    constructor(transactionId, itemType, data, handler) {
+    constructor(transactionId, itemType, dataId, data, handler) {
         super(transactionId, itemType, 'update', handler);
+        this._dataId = data;
         this._data = data;
+    }
+
+    get dataId() {
+        this._markAsRead('dataId');
+        return _dataId;
     }
 
     get data() {
