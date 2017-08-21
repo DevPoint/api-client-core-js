df305f2aac9e05497b9a2d8d65ab3d53533e6d00
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-21 12:40:42 +0200

    removed MapObserver from index.js

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,10 +1,8 @@
 
 export ArrayObserver from './ArrayObserver';
-export MapObserver from './MapObserver';
 export ObjectObserver from './ObjectObserver';
 export Observable from './Observable';
 export ObservableArray from './ObservableArray';
-export ObservableMap from './ObservableMap';
 export ObservableObject from './ObservableObject';
 export PropertyObserver from './PropertyObserver';
 export default Observer from './Observer';
