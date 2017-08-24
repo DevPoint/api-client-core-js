2688c339a1e4d7b32f7f736f1860a3d49c5242ed
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-24 14:21:39 +0200

    added index.js with all public exports

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+export ApiClient from './ApiClient';
+export ApiDispatcher from './ApiDispatcher';
+
+export 
+	Builder,
+	RelationBuilder } from './builder';
+
+export 
+	CacheDispatcher,
+	CacheDispatcherFactory } from './cache';
+
+export 
+	ArrayObserver,
+	ObjectObserver,
+	PropertyObserver
+	Observer,
+	ObservableArray,
+	ObservableObject,
+	Observable } from './observable';
+
+export {
+	InsertTransaction,
+	UpdateTransaction,
+	DeleteTransaction,
+	LoginTransaction,
+	RegisterTransaction,
+	Transaction,
+	TransactionDispatcher,
+	TransactionDispatcherFactory } from './transaction';
+
+export {
+	View,
+	ViewDispatcher,
+	ViewDispatcherFactory } from './view';
+
+export default Api from './Api';
