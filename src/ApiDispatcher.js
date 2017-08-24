5caab5a34b50ba7c563802f0b905b54ce8a0a485
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-24 10:08:52 +0200

    implemented ApiDispatcher.js

--- src/ApiDispatcher.js
+++ src/ApiDispatcher.js
@@ -13,12 +13,33 @@ class ApiDispatcher {
 
     constructor(api) {
         this._api = api;
-        this._insertTransactionDispatcher = this._createInsertTransactionDispatcher();
-        this._updateTransactionDispatcher = this._createUpdateTransactionDispatcher();
-        this._deleteTransactionDispatcher = this._createDeleteTransactionDispatcher();
-        this._loginTransactionDispatcher = this._createLoginTransactionDispatcher();
-        this._registerTransactionDispatcher = this._createRegisterTransactionDispatcher();
+        this._cacheDispatchers = this._createCacheDispatchers();
+        this._transactionDispatchers = this._createTransactionDispatchers();
         this._viewDispatcher = this._createViewTransactionDispatcher();
+    }
+
+    _createCacheDispatchers() {
+        const cacheDispatchers = {};
+        return cacheDispatchers;
+    }
+
+    _getCacheDispatcher($itemType) {
+        return this._cacheDispatchers[$itemType];
+    }
+
+    _createTransactionDispatchers() {
+        const transactionDispatchers = {
+            'insert': this.__createInsertTransactionDispatcher(),
+            'update': this._createUpdateTransactionDispatcher(),
+            'delete': this._createDeleteTransactionDispatcher(),
+            'login': this._createLoginTransactionDispatcher(),
+            'register': this._createRegisterTransactionDispatcher()
+        };
+        return transactionDispatchers;
+    }
+
+    _getTransactionDispatcher($type) {
+        return this._transactionDispatchers[$type];
     }
 
     _createInsertTransactionDispatcher() {
@@ -46,6 +67,22 @@ class ApiDispatcher {
     }
 
     dispatch(action) {
-
+        const actionTypeFrags = action.type.split('_');
+        if (actionTypeFrags.length >= 2) {
+            if (actionTypeFrags[1] === 'TRANSACTION') {
+                const transactionType = actionTypeFrags[2].toLowerCase();
+                const transactionDispatcher = this._getTransactionDispatcher(transactionType);
+                transactionDispatcher.dispatch(api.transactions, action);
+            }
+            else if (actionTypeFrags[1] === 'VIEW') {
+                const viewDispatcher = this._getViewDispatcher();
+                viewDispatcher.dispatch(api.views, action);
+            }
+            else if (actionTypeFrags[1] === 'CACHE') {
+                const itemType = actionTypeFrags[2].toLowerCase();
+                const cacheDispatcher = this._getCacheDispatcher(itemType);
+                cacheDispatcher.dispatch(api.cache(itemType), action);
+            }
+        }
     }
 }
