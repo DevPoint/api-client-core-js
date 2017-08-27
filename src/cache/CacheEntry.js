db06ebc2a40dfc9ac92b848ffe2a1b35921644d4
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-27 06:06:47 +0200

    added CacheEntry

---
+++ src/cache/CacheEntry.js
@@ -0,0 +1,29 @@
+
+import Observable from '../observable/Observable';
+import ObjectObserver from './observable/ObjectObserver';
+
+class CacheEntry extends Observable { 
+
+    constructor() {
+        super();
+        this._observer = this._createObserver();
+        this._observerLocked = true;
+    }
+
+    _createObserver() {
+        return new ObjectObserver();
+    }
+
+    markAsChanged() {
+        this._observer.markAsChanged();
+        if (this._parentObserver !== null) {
+            this._parentObserver.markAsChanged();
+        }
+    }
+
+    toObject() {
+        return {};
+    }
+}
+
+default export CacheEntry;
