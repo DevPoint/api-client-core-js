af539e95b3bf1ac22ed98cd3295908e2d13097ae
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-27 07:32:40 +0200

    added propTypes.js

---
+++ src/observable/propTypes.js
@@ -0,0 +1,27 @@
+
+export default {
+    'string': {
+        name: 'string',
+        default: ''
+    },
+    'number': {
+        name: 'number',
+        default: 0 
+    },
+    'boolean': {
+        name: 'number',
+        default: false 
+    },
+    'object': {
+        name: 'object',
+        default: {} 
+    },
+    'symbol': {
+        name: 'symbol',
+        default: Symbol() 
+    },
+    'array': {
+        name: 'array',
+        default: []
+    }
+}
