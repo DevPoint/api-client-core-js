f1f4a674178809c5d13ca406892cf2f313a37ac6
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-28 09:02:04 +0200

    added readOnly and setter

--- src/observable/ObservablePropType.js
+++ src/observable/ObservablePropType.js
@@ -1,11 +1,27 @@
 
-function CachePropType(name, default, base) {
+function ObservablePropType(name, default, base) {
     this.base = base;
     this.name = name;
     this.default = default;
+    this._readOnly = false;
+    this._setter = '';
     get typeOfName() {
         return this.base ? this.base.typeOfName : this.name;
     }
+    get readOnly() {
+    	return this._readOnly;
+    }
+    setReadOnly(value) {
+    	this._readOnly = value;
+    	return this;
+    }
+    get setter() {
+    	return this._setter;
+    }
+    setSetter(value) {
+    	this._setter = value;
+    	return this;
+    }
 }
 
-export default CachePropType;
+export default ObservablePropType;
