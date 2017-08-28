f8c937d141775faf70f0ea15f8fbde62745ae80f
Wilfried Reiter <wilfried.reiter@devpoint.at>
2017-08-28 09:55:46 +0200

    fixed export

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ export ApiDispatcher from './ApiDispatcher';
 
 export * from './builder';
 export * from './cache';
-export * from './cache/proptypes';
+export * from './observable/proptypes';
 export * from './observable';
 export * from './transaction';
 export * from './view';
 
-export observablePropTypes from './observable/proptypes';
 export cacheActions from './cache/actions';
+export observablePropTypes from './observable/proptypes';
 export transactionActions from './transaction/actions';
 export viewActions from './view/actions';
